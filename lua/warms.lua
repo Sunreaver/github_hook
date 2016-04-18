c724926c7f9cfe7144843dca6b379dfe17b15655
Peter <tanwei.rush@gmail.com>
2016-04-18 16:23:04 +0800

    table名称判断错误

--- lua/warms.lua
+++ lua/warms.lua
@@ -1,19 +1,19 @@
 
-DIR_WARMS = "$GOPATH/src/github.com/sunreaver/warms/"
-
 local lfs = require"lfs"
+local DIR_WARMS = "$GOPATH/src/github.com/sunreaver/warms/"
 
 -- 查找.go文件
 function attrdir (path)
 	local rp = io.popen("echo ".. path)
 	local realPath = rp:read("*l")
+	print(realPath)
 	local fNames = {}
 	local i = 1
     for file in lfs.dir(realPath) do
         if file ~= "." and file ~= ".." then
             local f = realPath..'/'..file
             local attr = lfs.attributes (f)
-            if type(attr) == table then
+            if type(attr) == "table" then
 	            -- assert (type(attr) == "table")
 	            -- if attr.mode == "directory" then
 	            if attr.mode == "file" then
@@ -41,13 +41,13 @@ function gitpull()
 end
 
 function buildAndDo(fileNames)
-	for j,item in pairs(fileNames) do
+	for j=1,#fileNames do
 		local exc = {}
 		exc[1] = "cd " .. DIR_WARMS
-		exc[2] = "go build " .. item
-		print("build: " .. item)
-		if string.find(item, "^huaban_warm.*%.go$") then
-			exc[3] = "mv " .. string.sub(item, 0, string.len(item) - 3) .. " /root/Doc/bin/huaban/huaban_warm"
+		exc[2] = "go build " .. fileNames[j]
+		print("build: " .. fileNames[j])
+		if string.find(fileNames[j], "^huaban_warm.*%.go$") then
+			exc[3] = "mv " .. string.sub(fileNames[j], 0, string.len(fileNames[j]) - 3) .. " /root/Doc/bin/huaban/huaban_warm"
 			print(exc[3])
 			exc[4] = "service huaban restart"
 		end
@@ -63,6 +63,7 @@ end
 -- do
 gitpull()
 local fns = attrdir(DIR_WARMS)
+print(#fns)
 -- assert(type(fns) == "table")
 buildAndDo(fns)
 
