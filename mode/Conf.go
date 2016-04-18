367fa3f7169f60a0800c1b0c2a779c95d87fb65a
TheOne <tanwei.rush@gmail.com>
2016-04-18 09:41:50 +0800

    init

---
+++ mode/Conf.go
@@ -0,0 +1,21 @@
+package mode
+
+import (
+	"encoding/json"
+)
+
+// Conf 配置信息
+type Conf struct {
+	Name  string `json:"name"`
+	DoLua string `json:"dolua"`
+}
+
+// MakeConf CreateConf
+func MakeConf(data []byte) (c []Conf) {
+	e := json.Unmarshal(data, c)
+	if e != nil {
+		c = []Conf{}
+		return
+	}
+	return
+}
